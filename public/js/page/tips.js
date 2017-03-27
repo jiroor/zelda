85af3e36be9cd396be63e45a2589fa5533bdd350
jiroor <thx.1by1.or25@gmail.com>
2017-03-27 21:27:39 +0900

    show all tips

--- public/js/page/tips.js
+++ public/js/page/tips.js
@@ -2,6 +2,38 @@
 	'use strict';
 
 	var tips = {
+		data: function() {
+			var self = this;
+
+			return {
+				tips: []
+			};
+		},
+
+		created: function() {
+			var self = this;
+			var promises = {
+				tips: self.getTips()
+			};
+
+			Promise.props(promises);
+		},
+
+		methods: {
+			getTips: function() {
+				var self = this;
+				var req = {
+					url: '/zelda/tips',
+					data: {}
+				};
+
+				return using('fetch').api(req)
+					.then(function(data) {
+						self.tips = data;
+					});
+			}
+		},
+
 		template: '#template--page--tips'
 	};
 
