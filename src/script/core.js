aa6bb5f24187b19a4db7baf21c99e593ead0fb87
jiroor <thx.1by1.or25@gmail.com>
2017-03-28 18:30:14 +0900

    load template files synchronously

--- src/script/core.js
+++ src/script/core.js
@@ -1,5 +1,27 @@
 (function(global) {
 	'use strict';
 
-	w3IncludeHTML();
+	var els = document.getElementsByTagName("*");
+	var el;
+	var file;
+	var request;
+
+	for (var i = 0; i < els.length; i++) {
+		el = els[i];
+		file = el.getAttribute("w3-include-html");
+
+		if (!file) {
+			continue;
+		}
+
+		request = new XMLHttpRequest();
+		request.open("GET", file, false);
+		request.send();
+
+		if (request.readyState == 4 && request.status == 200) {
+			el.outerHTML = request.responseText;
+			el.removeAttribute("w3-include-html");
+		}
+	}
+
 })(window);
